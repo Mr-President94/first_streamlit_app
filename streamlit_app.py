3f0be37a14b14ee0a4dd10ab52273d7adbc6338b
Rohit Amalnerkar <59511241+Mr-President94@users.noreply.github.com>
2023-04-27 15:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd
 import requests
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parent''s New Healthy Diner')
@@ -49,23 +50,32 @@ except URLError as e:
 
 
 # don't run anything beyond this point
-streamlit.stop()
+# streamlit.stop()
+streamlit.header('The Fruit Load List contains:')
 
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor() 
+def get_fruit_load _list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
 
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+# Add a button to load the fruits
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+
 # my_data_rows = my_cur.fetchone() -- fetches 1st record
-my_data_rows = my_cur.fetchall()
+# my_data_rows = my_cur.fetchall()
 
 # streamlit.text("The Fruit Load List contains:")
 # streamlit.text(my_data_row)
-streamlit.header('The Fruit Load List contains:')
-streamlit.dataframe(my_data_rows)
 
-fruit_choice_2 = streamlit.text_input('What fruit would you like add?','Kiwi')
-streamlit.write('Thanks for choosing ', fruit_choice_2)
+# streamlit.dataframe(my_data_rows)
+
+# fruit_choice_2 = streamlit.text_input('What fruit would you like add?','Kiwi')
+# streamlit.write('Thanks for choosing ', fruit_choice_2)
 
 
 
